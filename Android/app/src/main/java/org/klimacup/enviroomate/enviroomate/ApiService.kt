f17e2be95e9abb0330db74bf0cea2af7da9496dc
Timo Volk <timo.volk@gmail.com>
2018-05-25 11:54:30 +0200

    android init

--- Android/app/src/main/java/org/klimacup/enviroomate/enviroomate/ApiService.kt
+++ Android/app/src/main/java/org/klimacup/enviroomate/enviroomate/ApiService.kt
@@ -1,2 +1,39 @@
 package org.klimacup.enviroomate.enviroomate
 
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.os.IBinder
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.net.URL
+
+class ApiService : AsyncTask<URL, Integer, JSONObject>() {
+
+
+
+    override fun onProgressUpdate(vararg values: Integer?) {
+        super.onProgressUpdate(*values)
+    }
+
+    override fun onPostExecute(result: JSONObject?) {
+        super.onPostExecute(result)
+    }
+
+    override fun doInBackground(vararg params: URL?): JSONObject {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCancelled(result: JSONObject?) {
+        super.onCancelled(result)
+    }
+
+    override fun onCancelled() {
+        super.onCancelled()
+    }
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+    }
+}
