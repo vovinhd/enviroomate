c6a31177671944ac6f475e8e4e7731b35c6dedbb
Timo Volk <timo.volk@gmail.com>
2018-05-28 13:05:30 +0200

    haaands

--- pwa/src/index.ts
+++ pwa/src/index.ts
@@ -1,9 +1,12 @@
 
 import * as _ from "lodash";
+import './styles.scss';
+import 'materialize-css/sass/materialize.scss'
 
 function component() {
     let el = document.createElement('div');
     el.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    el.classList.add('hello');
     return el;
 }
 
@@ -12,6 +15,7 @@ document.body.appendChild(component());
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            registration.pushManager.subscribe({userVisibleOnly: true});
             console.log("SW registered: ", registration);
         }).catch(registrationError => {
             console.log("SW Registration failed! ", registrationError);
--- pwa/webpack.config.js
+++ pwa/webpack.config.js
@@ -1,30 +1,70 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.ts'
+        app: './src/index.ts',
     },
     module: {
         rules: [
             {
+                rules: [{
+                    test: /\.scss$/,
+                    use: [
+                        "style-loader", // creates style nodes from JS strings
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS
+                    ]
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+
+            {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },
     resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
+        extensions: ['.tsx', '.ts', '.js']
     },
     plugins: [
-        new WorkboxPlugin.GenerateSW({
-            clientsClaim: true,
-            skipWaiting: true
-        })
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            entry: 'html',
+            title: 'PWA',
+            template: 'src/index.html'
+        }),
+        new WorkboxPlugin.InjectManifest({
+            swSrc: 'src/sw.js',
+            swDest: 'service-worker.js',
+        }),
+        new CopyWebpackPlugin([ {from: './src/manifest.json', to: 'manifest.json'} ],  { debug: 'info' })
     ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
